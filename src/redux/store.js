71e7beacac09a4524e7dfb61c2ce95bf426403d7
gn.natalia19@gmail.com <gn.natalia19@gmail.com>
2022-01-31 22:36:22 +0200

    Add redux-saga

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,11 @@
-import {createStore, compose} from 'redux';
+import {createStore, compose, applyMiddleware} from 'redux';
 import { persistStore, persistReducer } from 'redux-persist'
-import storage from 'redux-persist/lib/storage'
+import storage from 'redux-persist/lib/storage';
+import createSagaMiddleware from 'redux-saga';
 
 import rootReducer from './rootReducer';
 
-
+//localStorage
 const persistConfig = {
   key: 'tasks',
   storage,
@@ -12,11 +13,17 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
+//middlewares
+
+const sagaMiddleware = createSagaMiddleware();
+const middlewares = [sagaMiddleware];
+
 export const store = compose(
+  applyMiddleware(...middlewares),
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )(createStore)(persistedReducer);
 
-
+//sagaMiddleware.run(rootSaga)
 
 export const persistor = persistStore(store);
 
